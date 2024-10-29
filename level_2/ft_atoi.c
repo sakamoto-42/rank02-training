306f9344989ec0de6315852e490186e130e668fc
julien <julien66140@hotmail.fr>
2024-10-29 17:00:20 +0100

    ft_atoi

---
+++ level_2/ft_atoi.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 16:48:07 by sakamoto-42       #+#    #+#             */
+/*   Updated: 2024/10/29 16:59:40 by sakamoto-42      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
+}
+
+/*
+int	main(int argc, char **argv)
+{
+	if (argc - 1 == 1)
+	{
+		printf("%s\n", argv[1]);
+		printf("%d\n", ft_atoi(argv[1]));
+	}
+	return (0);
+}
+*/
