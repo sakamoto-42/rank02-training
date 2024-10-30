daef30c89dce7093276daad9b314a5d5805af44c
julien <julien66140@hotmail.fr>
2024-10-30 09:28:30 +0100

    strcpy added

--- level_1/ft_strcpy.c
+++ level_1/ft_strcpy.c
@@ -6,11 +6,10 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 07:40:46 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/10/29 08:02:49 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/10/30 09:28:06 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 //#include <stdlib.h>
 //#include <stdio.h>
 //#include <string.h>
@@ -33,14 +32,18 @@ char	*ft_strcpy(char *s1, char *s2)
 int	main(int argc, char **argv)
 {
 	char	*src;
-	char	*dest;
+	char	*dest1;
+	char	*dest2;
 
 	if (argc - 1 != 1)
 		return (1);
 	src = argv[1];
-	dest = (char *) malloc((strlen(src) + 1) * sizeof(char));
-	ft_strcpy(dest, src);
-	printf("src : %s\ndest: %s\n", src, dest);
+	dest1 = (char *) malloc((strlen(src) + 1) * sizeof(char));
+	strcpy(dest1, src);
+	printf("strcpy :\nsrc : %s\ndest: %s\n", src, dest1);
+	dest2 = (char *) malloc((strlen(src) + 1) * sizeof(char));
+	ft_strcpy(dest2, src);
+	printf("ft_strcpy :\nsrc : %s\ndest: %s\n", src, dest2);
 	return (0);
 }
 */
