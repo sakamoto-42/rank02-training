0a620401e38e5e1a004711b1784d9802450c0677
julien <julien66140@hotmail.fr>
2024-10-30 09:20:56 +0100

    strcmp v2

--- level_2/ft_strcmp.c
+++ level_2/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:03:02 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/10/29 17:14:39 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/10/30 09:20:39 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,12 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	unsigned char	c1;
-	unsigned char	c2;
-
-	while (*s1 || *s2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		c1 = (unsigned char) *s1;
-		c2 = (unsigned char) *s2;
-		if (c1 != c2)
-			return (c1 - c2);
 		s1++;
 		s2++;
 	}
-	return (0);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
 
 /*
